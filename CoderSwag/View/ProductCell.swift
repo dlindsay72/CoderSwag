799008f609a3a145dcb4ad2f11956aba6e0ad412
Dan Lindsay <dlindsay72@hotmail.com>
2017-09-13 17:25:21 -0400

    collection view ProductCell created with IBOutlets connected in custom class

---
+++ CoderSwag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  CoderSwag
+//
+//  Created by Dan Lindsay on 2017-09-13.
+//  Copyright © 2017 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+
+public var productCellIdentifier = "productCell"
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
